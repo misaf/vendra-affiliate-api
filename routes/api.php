f94a57c9a95c329471c219999daa8c057ee295de
misaf <misaf.1990@gmail.com>
2026-07-16 21:37:18 +0000

    refactor(api): decouple domain APIs from localization

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
 use LaravelJsonApi\Laravel\Http\Controllers\JsonApiController;
 use LaravelJsonApi\Laravel\Routing\ResourceRegistrar;
 
-Route::middleware(['api', 'vendra.locale'])->group(function (): void {
+Route::middleware('api')->group(function (): void {
     JsonApiRoute::server('vendra-affiliate')->prefix('v1')->resources(function (ResourceRegistrar $server): void {
         $server->resource('affiliates', JsonApiController::class)
             ->readOnly();
